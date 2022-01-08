83d70bad4a5a311f3d80bc256176abaee3901b6e
JunHong <alswnsghd1234@naver.com>
2022-01-09 02:39:03 +0900

    update

--- pr_community/pruser/admin.py
+++ pr_community/pruser/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Pruser
 # Register your models here.
+
+class PruserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'password')
+
+admin.site.register(Pruser, PruserAdmin)
--- pr_community/pruser/models.py
+++ pr_community/pruser/models.py
@@ -9,9 +9,14 @@ class Pruser(models.Model):
                                 verbose_name='비밀번호')    # dttm은 datetime의 약자로 사용!
     registered_dttm = models.DateTimeField(auto_now=True,  # auto_now_add=True 자동으로 현시간 저장! /Flask에 비해 편리
     verbose_name='등록시간')
+    
+    def __str__(self):
+        return self.username # 새로 만든 유저 문구는 클래스를 문자열로 변환했을때 나오는값이지만
+                            # __str__ 이라는 내장함수를 파이썬은 갖고 있다
 
     class Meta:
         db_table = 'pruser_pruser' # 기본적으로 추가되는 앱들과 구분하기 위해 생성
-
+        verbose_name = '프로젝트 사용자'
+        verbose_name_plural = '프로젝트 사용자'
                 
 
