ab6684a9e3c6dce78dde1e50d31fac593a6fe81e
JunHong <alswnsghd1234@naver.com>
2022-01-09 03:30:01 +0900

    update

--- pr_community/pruser/views.py
+++ pr_community/pruser/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .models import Pruser
 
 # Create your views here.
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        re_password = request.POST['re-password']
+        
+        pruser = Pruser(
+            username=username,
+            password=password
+        )
+        pruser.save()
+        return render(request, 'register.html')
