5ca33aee86c6b4cde58a0a9728b03261a4a3db47
JunHong <alswnsghd1234@naver.com>
2022-01-18 13:00:03 +0900

    board login

--- pr_community/board/views.py
+++ pr_community/board/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Board
+from .forms import BoardForm
 
 # Create your views here.
+def board_write(request):
+    form = BoardForm()
+    return render(request, 'board_write.html',{'form':form})
 
 def board_list(request):
     boards = Board.objects.all().order_by('-id')
