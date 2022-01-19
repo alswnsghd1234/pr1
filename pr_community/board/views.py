3356c1fae4ebecd882d6135d2ac888928db981ec
JunHong <alswnsghd1234@naver.com>
2022-01-19 16:58:15 +0900

    update

--- pr_community/board/views.py
+++ pr_community/board/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from pruser.models import Pruser
 from .models import Board
 from .forms import BoardForm
 
 # Create your views here.
+
+
+def board_detail(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request, 'board_detail.html', {'board':board})
+
+
 def board_write(request):
-    form = BoardForm()
+    if request.method == 'POST':
+        form = BoardForm(request.POST)
+        if form.is_valid():
+            user_id = request.session.get('user')
+            pruser = Pruser.objects.get(pk=user_id)
+            
+            board = Board()
+            board.title = form.cleaned_data['title']
+            board.contents = form.cleaned_data['contents']
+            board.writer = pruser
+            board.save()
+            
+            return redirect('/board/list/')
+    
+    else:
+        form = BoardForm()
+        
     return render(request, 'board_write.html',{'form':form})
 
 def board_list(request):
